6edf200dd3fcc0521010ce7a95d0b581565ea123
Mark Malabanan <mark.malabanan24@gmail.com>
2020-12-02 15:52:09 -0500

    Initial commit

---
+++ publish/precache-manifest.acddc8d0cf5d8a30b9aa6a7e07cc5e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d946168fb18ebbc74cad97f74f851d80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62ccfad28d772d8a4480",
+    "url": "/static/css/main.92646c23.chunk.css"
+  },
+  {
+    "revision": "d6734d0ebd5f4616be83",
+    "url": "/static/js/2.1a0a8e3f.chunk.js"
+  },
+  {
+    "revision": "70790f78a7441fa7f267aa40d580a2ec",
+    "url": "/static/js/2.1a0a8e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ccfad28d772d8a4480",
+    "url": "/static/js/main.8545ce21.chunk.js"
+  },
+  {
+    "revision": "fdbb502898d21f88e428",
+    "url": "/static/js/runtime-main.133988cf.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
